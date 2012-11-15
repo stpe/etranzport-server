23f2f0dbf8c1c1e41959dac3de7deb18956e9b79
Stefan Pettersson <stefan@plan3.se>
2012-11-15 22:28:34 +0100

    Navbar time factor component css fix

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
 	<div class="navbar-inner">
 		<div class="container">
 			<span class="brand">eTranzport Experiments</span>
-			<div class="input-prepend input-append pull-right">
+			<div class="navbar-control input-prepend input-append pull-right">
 				<span class="add-on">Time Factor</span><input type="text" class="span1" id="timeFactor"><button class="btn" type="button" id="timeFactorDec">&lt;</button><button class="btn" type="button" id="timeFactorInc">&gt;</button>
 			</div>
 		</div>
