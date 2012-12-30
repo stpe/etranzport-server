9361b6346f90865f2b85d8a30a9e7fbf6e48febb
Stefan Pettersson <stefan@plan3.se>
2012-12-30 23:17:13 +0100

    Data loader class

---
+++ api/data.php
@@ -0,0 +1,56 @@
+<?php
+
+// Exceptions
+
+class DataException extends RuntimeException {
+
+    public function __construct($message, $code = 0, Exception $previous = null) {
+        parent::__construct($message, $code, $previous);
+
+        $this->log();
+    }
+
+    private function log() {
+        $msg = $this->getMessage();
+        error_log($msg);
+    }
+}
+
+class DataFileException extends DataException {
+
+}
+
+class DataFormatException extends DataException {
+
+}
+
+// Main class
+
+class Data {
+    private $data;
+
+    function __construct($filename) {
+        $this->load($filename);
+    }
+
+    private function load($filename) {
+        $contents = file_get_contents($filename);
+
+        if (!$contents) {
+            throw new DataFileException("Can't open data file '$filename'");
+        }
+
+        $decoded = json_decode($contents, true);
+        if (!$decoded) {
+            throw new DataFormatException("Can't parse data file '$filename', error: " . json_last_error());
+        }
+
+        $this->data = $decoded;
+    }
+
+    public function out() {
+        echo json_encode($this->data, JSON_PRETTY_PRINT);
+    }
+}
+
+// Specific data type implementations
